6a61a1f8d199c6b9ba2bc8a7404cd9512cc2fcaf
Dale <Dale@Dale-PC>
2016-02-28 17:21:40 +0600

    Финал. Без юнитов

--- src/com/grs24/mt/unistream/wsclient/GetDocumentType.java
+++ src/com/grs24/mt/unistream/wsclient/GetDocumentType.java
@@ -38,7 +38,127 @@ public class GetDocumentType {
    
     /**
 * Выполнение запроса на получение ID типа документа
-* @param code
+* @param code. Поддерживаемые типы документов:
+* Военный билет
+Вид на жительство
+Паспорт моряка
+Водительские права
+Удостоверение МВД
+UK Driving Licence
+Карточка иностранца
+ИНН
+Кредитная карта
+Миграционная карта
+Виза
+Приглашение
+Иной документ
+Свидетельство о предоставлении временного убежища
+Свидетельство на возвращение в страну гражданства
+Текущий запрос о выписке налогооблажения
+Общегражданский заграничный паспорт
+Временное удостоверение личности
+Удостоверение личности военнослужащего
+Идентификационная карта СНГ
+Идентификационная карта
+Государственное удостоверение личности
+Сертификат о натурализации
+Номер социального обеспечения
+Паспорт иностранного гражданина
+Счет за коммунальные услуги
+Текущая выписка из банковского счета
+Выписка с кредитной/дебетной карты
+Текущий запрос о выписке налогооблажения
+Судебная повестка
+Свидетельство  органов  ЗАГС
+Разрешение на временное проживание
+Свид-во о рассмотрении признания лица беженцем
+Удостоверение беженца
+Паспорт гражданина РФ
+Временное удостоверение личности гражданина РФ
+Вид на жительство в РФ
+Дипломатический паспорт
+Разрешение на временное проживание в РФ
+Свидетельство на возвращение в страну гражданства
+Удостоверение личности гражданина иностр. гос-ва
+Свидетельство о предоставлении временного убежища
+Св-во о предоставлении вр. убежища на терр. РФ
+Другой документ
+Паспорт
+Временное удостоверение личности
+Вид на жительство (Украина)
+Разрешение на временное проживание в РФ (без гр.)
+Вид на жительство (Белоруссия)
+Разр. на временное проживание в РФ (лиц без гр.)
+Вид на жительство (Белоруссия)
+Паспорт (Белоруссия)
+Удостоверение беженца (Белоруссия)
+Виза (Белоруссия)
+Паспорт (Казахстан)
+Удостоверение личности (Казахстан)
+Вид на жительство иностранного граждан. Казахстан
+Паспорт лица без гражданства (Казахстан)
+Вид на жительство лица без гражданства в РФ
+Справка об освобождении из мест лишения свободы
+* English variant
+* Military ID
+Residence permit
+Overseas Passport
+Driving Licence
+Home Office Ind
+UK Driving Licence
+Alien Card
+Tax Id
+Credit Card
+Immigration Permit
+Visa
+Invitation
+Other ID
+Certificate of laissez-passer  citizenship
+Current council tax demand letter, or statement
+foreign passport
+Pink Slip
+ID
+CIS Card
+ID Card
+State Identification Card
+Naturalization Certificate
+Social Security
+Foreign passport
+Utility Bill
+Current bank statement
+Credit /debit card statement
+Current council tax demand letter, or statement
+Instrument of a court appointment
+Registry office certificate
+Sojourn permit
+Certificate of consideration of refugee status rec
+Refugee Certificate
+Russian Federation Passport
+Russian Federation temporary identity card
+Residence permit in Russian Federation
+Diplomatic passport
+Temporary residence permit in Russia
+Certificate of laissez-passer  citizenship
+Foreign country ID
+.
+Certificate for temporary asylum in Russian Federa
+Another document
+Passport
+Interim Certificate
+Residence permit in Ukraine
+Temporary residence permit in Russia (stless)
+Residence permit (Belarus)
+Temp. residence permit in Russ (stateless pers.)
+Residence permit (Belarus)
+Passport (Belarus)
+Refugee certificate (Belarus)
+Visa (Belarus)
+Passport (Kazakhstan)
+Id (Kazakhstan)
+ResidencePermit (Kazakhstan)
+StatelessPassport (Kazakhstan)
+Stateless person residence permit in Russian
+Inprisonment release certificate in Russian
 * @return ID тпа документа
 * 
 * @throws Exception в случае провала выполение
