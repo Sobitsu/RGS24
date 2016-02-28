e30e452f609f2945b5add7e7c60b2957e8c73e34
Dale <Dale@Dale-PC>
2016-02-28 15:41:06 +0600

    Финал. Без юнитов

--- test/com/grs24/mt/unistream/wsclient/GetTransferByIDTest.java
+++ test/com/grs24/mt/unistream/wsclient/GetTransferByIDTest.java
@@ -5,6 +5,9 @@
  */
 package com.grs24.mt.unistream.wsclient;
 
+import com.grs24.mt.unistream.MtUnistreamAdapter;
+import java.io.IOException;
+import java.util.Properties;
 import org.datacontract.schemas._2004._07.wcfservicelib.GetTransferByIDResponseMessage;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,11 +22,23 @@ import static org.junit.Assert.*;
  */
 public class GetTransferByIDTest {
     
-    public GetTransferByIDTest() {
-    }
-    
+ public static String KEY_USER_AUTHED_APIKEY = "1wwteyFGFew624";
+    public static String KEY_USER_AUTHED_LOGIN = "g2.grstwentyfour.rus";
+    public static String KEY_USER_AUTHED_PASSWORD = "7!LrO7i7";
+    public static Integer KEY_BANK_ID = 383589;
+    public static Integer KEY_PARTICIPATOR_ID = 383589;
+    private static MtUnistreamAdapter instance;
+  
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws IOException {
+        Properties cfg = new Properties();
+        cfg.setProperty("APIKEY", KEY_USER_AUTHED_APIKEY);
+        cfg.setProperty("LOGIN", KEY_USER_AUTHED_LOGIN);
+        cfg.setProperty("PASSWORD", KEY_USER_AUTHED_PASSWORD);
+        cfg.setProperty("BANKID", KEY_BANK_ID.toString());
+        cfg.setProperty("PARTID",KEY_PARTICIPATOR_ID.toString());
+        instance = new MtUnistreamAdapter();
+        instance.init(cfg);    
     }
     
     @AfterClass
@@ -44,12 +59,13 @@ public class GetTransferByIDTest {
     @Test
     public void testGetTransferByID() throws Exception {
         System.out.println("getTransferByID");
-        Integer transferId = null;
-        GetTransferByIDResponseMessage expResult = null;
+        Integer transferId = 16015170;
         GetTransferByIDResponseMessage result = GetTransferByID.getTransferByID(transferId);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertTrue(result.getFault().isNil());
+        assertFalse(result.getTransfer().isNil());
+        assertNotNull(result.getTransfer().getValue());
+        assertFalse(result.getTransfer().getValue().getAmounts().isNil());
+        assertEquals(result.getTransfer().getValue().getControlNumber().getValue(),"828105025765");
+        System.out.println("getTransferByID OK");
     }
-    
 }
