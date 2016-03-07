2e911536bf6b10dd7cc385c5c3b1015d471977d9
Dale <Dale@Dale-PC>
2016-03-07 14:47:19 +0600

    Финал. Без юнитов

--- test/com/grs24/mt/unistream/wsclient/CreatePersonTest.java
+++ test/com/grs24/mt/unistream/wsclient/CreatePersonTest.java
@@ -12,7 +12,10 @@ import com.grs24.msg.IndividualHolder;
 import com.grs24.msg.PersonHolder;
 import com.grs24.mt.unistream.DateTimeUtils;
 import com.grs24.mt.unistream.MtUnistreamAdapter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Properties;
 import javax.xml.namespace.QName;
 import org.datacontract.schemas._2004._07.wcfservicelib.CreatePersonResponseMessage;
@@ -29,18 +32,33 @@ import static org.junit.Assert.*;
  * @author Dale
  */
 public class CreatePersonTest {
-    
-    public static String KEY_USER_AUTHED_APIKEY = "1wwteyFGFew624";
-    public static String KEY_USER_AUTHED_LOGIN = "g2.grstwentyfour.rus";
-    public static String KEY_USER_AUTHED_PASSWORD = "7!LrO7i7";
-    public static Integer KEY_BANK_ID = 383589;
-    public static Integer KEY_PARTICIPATOR_ID = 383589;
-    private static MtUnistreamAdapter instance;
+
     private final static QName _FirstName_QNAME = new QName("http://schemas.datacontract.org/2004/07/WcfServiceLib", "FirstName");
     private final static QName _LastName_QNAME = new QName("http://schemas.datacontract.org/2004/07/WcfServiceLib", "LastName");
     private final static QName _MiddleName_QNAME = new QName("http://schemas.datacontract.org/2004/07/WcfServiceLib", "MiddleName");
 
-
+    
+     private static final String KEY_USER_AUTHED_APIKEY = "1wwteyFGFew624";
+    private static final String KEY_USER_AUTHED_LOGIN = "g2.grstwentyfour.rus";
+    private static final String KEY_USER_AUTHED_PASSWORD = "7!LrO7i7";
+    private static final Integer KEY_BANK_ID = 383589;
+    private static final Integer KEY_PARTICIPATOR_ID = 383589;
+    private static final Integer KEY_SERVER_REQUEST_TUMEOUT = 3000;
+    private static final Integer KEY_SERVER_CONNECT_TUMEOUT = 1000;
+    private static final String KEY_KEYSTORE_PASSWORD = "123456";
+    private static final String KEY_KEY_PASSWORD = "1234567890";
+    private static final String KEY_TRUSTSTORE_PASSWORD = "123456";
+    private static final String KEYSTOREPATH = "xws-security/client.jks";
+    private static final String TRUSTSTOREPATH = "xws-security/server.jks";
+    private static MtUnistreamAdapter instance;
+   
+    private static String computeB64(String filename) throws IOException {
+        File target = new File(filename);
+        FileInputStream stream = (new FileInputStream(target));
+        byte[] buffer = new byte[stream.available()];
+        stream.read(buffer);
+        return Base64.getEncoder().encodeToString(buffer);
+    }
     
     @BeforeClass
     public static void setUpClass() throws IOException {
@@ -50,9 +68,17 @@ public class CreatePersonTest {
         cfg.setProperty("PASSWORD", KEY_USER_AUTHED_PASSWORD);
         cfg.setProperty("BANKID", KEY_BANK_ID.toString());
         cfg.setProperty("PARTID",KEY_PARTICIPATOR_ID.toString());
+        cfg.setProperty("SERVER.REQUEST_TIMEOUT", KEY_SERVER_REQUEST_TUMEOUT.toString());
+        cfg.setProperty("SERVER.CONNECT_TIMEOUT", KEY_SERVER_CONNECT_TUMEOUT.toString());
+        cfg.setProperty("KEYSTORE.PASSWORD", KEY_KEYSTORE_PASSWORD);
+        cfg.setProperty("KEY.PASSWORD", KEY_KEY_PASSWORD);
+        cfg.setProperty("TRUSTSTORE.PASSWORD", KEY_TRUSTSTORE_PASSWORD);
+        cfg.setProperty("KEYSTORE.PKCS12", computeB64(KEYSTOREPATH));
+        cfg.setProperty("TRUSTSTORE.JKS",  computeB64(TRUSTSTOREPATH));
         instance = new MtUnistreamAdapter();
         instance.init(cfg);    
     }
+
     public CreatePersonTest() {
     }
     
